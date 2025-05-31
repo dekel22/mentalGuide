053cd2630a42e957dc404e798652105d61c01e50
dekel22 <dekell8@gmail.com>
2025-06-01 01:22:54 +0300

    Update and rename test to test.js

---
+++ api/test.js
@@ -0,0 +1,19 @@
+const { MongoClient } = require('mongodb');
+
+const uri = process.env.MONGODB_URI;
+
+module.exports = async (req, res) => {
+  const client = new MongoClient(uri);
+  try {
+    await client.connect();
+    const db = client.db("myformdb");
+    const collection = db.collection("formdata");
+    await collection.insertOne({ name: "test", time: new Date() });
+    res.status(200).json({ success: true });
+  } catch (err) {
+    console.error("MongoDB error:", err);
+    res.status(500).json({ error: err.message, stack: err.stack });
+  } finally {
+    await client.close();
+  }
+};
